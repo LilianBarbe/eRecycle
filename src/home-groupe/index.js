5f2e0dae4263245b89f85b53cc1743573f635ac5
LilianBarbe <110779804+LilianBarbe@users.noreply.github.com>
2024-05-22 11:24:51 +0200

    delete initswip dans groupe

--- src/home-groupe/index.js
+++ src/home-groupe/index.js
@@ -2,8 +2,6 @@ import { animCarousel } from '$utils/carousel.js';
 
 document.addEventListener('DOMContentLoaded', function () {
   console.log('Home Groupe');
-  initSwiper();
+
   animCarousel();
 });
-
-window.addEventListener('resize', initSwiper);
